689ab161190c4a70aae4c4d62454d53e6854fadc
DaniilShd <ds_const@mail.ru>
2024-01-18 22:04:48 +0300

    test2

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,20 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
 
 func main() {
-	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	engine := html.New("./templates", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
 	})
+	app.Get("/", handlers.test)
 
-	app.Listen(":3000")
+	log.Fatal(app.Listen(":3000"))
 }
