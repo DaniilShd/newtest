ea30970f051837df1049bce262a0e0f19d6b9ed3
DaniilShd <ds_const@mail.ru>
2024-01-19 20:55:09 +0300

    test4

--- cmd/main.go
+++ cmd/main.go
@@ -7,13 +7,20 @@ import (
 	"github.com/gofiber/template/html/v2"
 )
 
+func test(c *fiber.Ctx) error {
+	return c.Render("index", fiber.Map{
+		"Title": "Hello, World!",
+	})
+}
+
 func main() {
 
-	engine := html.New("./templates", ".html")
+	engine := html.New("../templates", ".html")
 
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
+
 	app.Get("/", handlers.test)
 
 	log.Fatal(app.Listen(":3000"))
